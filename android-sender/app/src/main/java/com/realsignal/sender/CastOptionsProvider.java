b1e590ad4c7c2ac02350f02571d4179fbbeeb686
esfsfestgfse <54010214+esfsfestgfse@users.noreply.github.com>
2026-08-31 08:21:05 -0500

    Fix Cast device discovery

--- android-sender/app/src/main/java/com/realsignal/sender/CastOptionsProvider.java
+++ android-sender/app/src/main/java/com/realsignal/sender/CastOptionsProvider.java
@@ -6,17 +6,14 @@ import com.google.android.gms.cast.framework.CastOptions;
 import com.google.android.gms.cast.framework.OptionsProvider;
 import com.google.android.gms.cast.framework.SessionProvider;
 
-import java.util.Collections;
 import java.util.List;
 
 /** Supplies the published RealSignal Custom Web Receiver to the Cast framework. */
 public final class CastOptionsProvider implements OptionsProvider {
     @Override
     public CastOptions getCastOptions(Context context) {
-        List<String> namespaces = Collections.singletonList(MainActivity.CUSTOM_NAMESPACE);
         return new CastOptions.Builder()
                 .setReceiverApplicationId(context.getString(R.string.cast_app_id))
-                .setSupportedNamespaces(namespaces)
                 .build();
     }
 
@@ -25,4 +22,3 @@ public final class CastOptionsProvider implements OptionsProvider {
         return null;
     }
 }
-
